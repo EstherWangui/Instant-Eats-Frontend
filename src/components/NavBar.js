4262acf025a7604d4fac5993ca64a52d09de50b2
Anna-Ashton <ann.karanja@student.moringaschool.com>
2022-12-09 20:55:53 +0300

    included routes for logout

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,23 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 
 function NavBar({ onLogout }) {
     function handleLogOut(){
-        fetch("instant-eats-production.up.railway.app", {
+        fetch("/logout", {
             method: "DELETE",
         }).then(() => onLogout());
     }
 
     return (
         <header>
-            <button onClick={handleLogout}>Logout</button>
-        </header>
+            <button onClick={handleLogOut}>Logout</button>
+        <li className="nav-item">
+        <Link to='/home' className='nav-link'>Home</Link>
+      </li>
+      <li className="nav-item">
+          <Link to='/about' className='nav-link'>About</Link>
+        </li>
+      </header>
     );
 }
 
